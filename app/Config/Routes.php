953c1622af233134b617a9ed2094e96698d1e038
Fadli <muhammadfadli200509@gmail.com>
2026-04-26 00:27:41 +0700

    Capstone 4: CI4 - Session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,9 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
 
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');

5125e48a65f68d323902d3b4dee34c60d315c4f7
Fadli <muhammadfadli200509@gmail.com>
2026-04-26 00:25:03 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+
+
+$routes->get('/produk', 'ProdukController::index');
+$routes->get('/keranjang', 'TransaksiController::index');
